8924f53a5fc004c85ed76bb54c6b290f55fa0cb8
Andrew Baron <Drowelfofdead@gmail.com>
2016-04-06 13:33:40 -0400

    add working click function to AVED for pulling data

--- assets/js/perfAVEDcontroller.js
+++ assets/js/perfAVEDcontroller.js
@@ -30,5 +30,29 @@ TheSceneryapp.controller('perfAVEDcont', function($scope){
     }
   }//end scope.toggle
 
+  $scope.addperformance = function(){
+    // console.log($('.hero-img-creator-dropdown option:selected').text());
+    // console.log($('#ticket-link').val())
+
+
+    var performance = JSON.stringify({
+    "performance": {
+      "owner_id": $('.hero-img-creator-dropdown option:selected').text(),
+      "company_id": "1",
+      "name": $('#performance-name').val(),
+      "description": $('#perf-desc').val(),
+      "trailer_link": $('#trailer-link').val(),
+      "ticket_link": $('#ticket-link').val()
+    },
+    "user_info": {
+      "login_token":  "butts"//response.user_info.login_token
+    }
+
+    });
+    console.log(performance);
+
+
+  }//End addperformance
+
 
 });

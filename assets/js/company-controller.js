ac9f219764ff5a1b295892bddd8031298a33807e
Andrew Baron <Drowelfofdead@gmail.com>
2016-04-11 15:07:00 -0400

    company can create edit is under construction

--- assets/js/company-controller.js
+++ assets/js/company-controller.js
@@ -16,6 +16,8 @@ TheSceneryapp.controller('companyCont', function($scope, $http, ourData){
     $(".media-facebook").parent().attr("href", data.data.company.facebook_link);
     $(".media-instagram").parent().attr("href", data.data.company.instagram_link);
     $(".company-description").text(data.data.company.description);
+
+    localStorage.setItem("companyid", JSON.stringify(data.data.company.id));
   });
 
 
@@ -37,23 +39,23 @@ TheSceneryapp.controller('companyCont', function($scope, $http, ourData){
 
   // Set view variables to new edited variables.
 
-  $scope.savecompany = function(){
+  $scope.createcompany = function(){
 
     var createCompany = JSON.stringify({
     "company": {
       "id": "",
       "user_id": ownerID,
-      "name": $(".edit-company-name").val(),
-      "description": $(".edit-company-description").val(),
-      "website_link": $(".edit-company-url").val(),
-      "facebook_link": $(".edit-company-facebook").val(),
-      "twitter_link": $(".edit-company-twitter").val(),
-      "instagram_link": $(".edit-company-instagram").val(),
-      "youtube_link": $(".edit-company-youtube").val(),
-      "address": $(".edit-company-location-address").val(),
-      "city": $(".edit-company-location-city").val(),
-      "state": $(".edit-company-location-state option:selected").text(),
-      "zip_code": $(".edit-company-location-zip").val()
+      "name": $(".create-company-name").val(),
+      "description": $(".create-company-description").val(),
+      "website_link": $(".create-company-url").val(),
+      "facebook_link": $(".create-company-facebook").val(),
+      "twitter_link": $(".create-company-twitter").val(),
+      "instagram_link": $(".create-company-instagram").val(),
+      "youtube_link": $(".create-company-youtube").val(),
+      "address": $(".create-company-location-address").val(),
+      "city": $(".create-company-location-city").val(),
+      "state": $(".create-company-location-state option:selected").text(),
+      "zip_code": $(".create-company-location-zip").val()
     },
     "user_info": {
       "login_token": token
@@ -75,24 +77,79 @@ TheSceneryapp.controller('companyCont', function($scope, $http, ourData){
     };
 
     $.ajax(settings).done(function (data) {
-    console.log(data);
+      console.log("Company Created");
+      console.log(data);
     });//end ajax.
+  }//End Create Company
 
-    $(".company-name").text($(".edit-company-name").val());
-    $(".edit-company-name").val("");
-    $(".company-location").text($(".edit-company-location").val());
-    $(".edit-company-location").val("");
-    $(".company-url").parent().attr("href", ($(".edit-company-url").val()));
-    $(".edit-company-url").val("");
-    $(".media-youtube").parent().attr("href", $(".edit-company-youtube").val());
-    $(".edit-company-youtube").val("");
-    $(".media-twitter").parent().attr("href", $(".edit-company-twitter").val());
-    $(".edit-company-twitter").val("");
-    $(".media-facebook").parent().attr("href", $(".edit-company-facebook").val());
-    $(".edit-company-facebook").val("");
-    $(".media-instagram").parent().attr("href", $(".edit-company-instagram").val());
-    $(".edit-company-instagram").val("");
-    $(".company-description").text($(".edit-company-description").val());
-    $(".edit-company-description").val("");
-  }
+  $scope.savecompany = function(){
+
+    var editcompany = JSON.stringify({
+      "company": {
+        "id": JSON.parse(localStorage.getItem('companyid'),
+        "user_id": ownerID,
+        "name": $(".edit-company-name").val(),
+        "description": $(".edit-company-description").val(),
+        "website_link": $(".edit-company-url").val(),
+        "facebook_link": $(".edit-company-facebook").val(),
+        "twitter_link": $(".edit-company-twitter").val(),
+        "instagram_link": $(".edit-company-instagram").val(),
+        "youtube_link": $(".edit-company-youtube").val(),
+        "address": $(".edit-company-location-address").val(),
+        "city": $(".edit-company-location-city").val(),
+        "state": $(".edit-company-location-state option:selected").text(),
+        "zip_code": $(".edit-company-location-zip").val()
+      },
+      "user_info": {
+        "login_token": token
+      }
+    });
+
+    //AJAX CALL
+      // var settings = {
+      //   "async": true,
+      //   "crossDomain": true,
+      //   "url": "http://infinite-reef-76606.herokuapp.com/companies",
+      //   "method": "PATCH",
+      //   "headers": {
+      //     "content-type": "application/json",
+      //     "cache-control": "no-cache"
+      //   },
+      //   "processData": false,
+      //   "data": editcompany
+      //    };
+      //
+      //   $.ajax(settings).done(function (data) {
+      //     console.log("Updated Company");
+      //     console.log(data);
+      //   });//end ajax.
+  }//End Save Company
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".company-name").text($(".edit-company-name").val());
+// $(".edit-company-name").val("");
+// $(".company-location").text($(".edit-company-location").val());
+// $(".edit-company-location").val("");
+// $(".company-url").parent().attr("href", ($(".edit-company-url").val()));
+// $(".edit-company-url").val("");
+// $(".media-youtube").parent().attr("href", $(".edit-company-youtube").val());
+// $(".edit-company-youtube").val("");
+// $(".media-twitter").parent().attr("href", $(".edit-company-twitter").val());
+// $(".edit-company-twitter").val("");
+// $(".media-facebook").parent().attr("href", $(".edit-company-facebook").val());
+// $(".edit-company-facebook").val("");
+// $(".media-instagram").parent().attr("href", $(".edit-company-instagram").val());
+// $(".edit-company-instagram").val("");
+// $(".company-description").text($(".edit-company-description").val());
+// $(".edit-company-description").val("");

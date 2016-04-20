77c061e9550814aedf2bd9b62a6c5a979caa3ebf
byarsawich <byarsawich@gmail.com>
2016-04-20 17:51:07 -0400

    changed website links to have http:// instead of // because most won't handle https:.  social links are left alone because they all use https.

--- assets/js/company-controller.js
+++ assets/js/company-controller.js
@@ -292,7 +292,7 @@ TheSceneryapp.controller('companyCont', function($scope, $http, ourData, $window
       var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "http://infinite-reef-76606.herokuapp.com/companies/"+compID,
+        "url": "https://api.the-scenery.com/companies/"+compID,
         "method": "DELETE",
         "headers": {
           "content-type": "application/json",
@@ -310,8 +310,8 @@ TheSceneryapp.controller('companyCont', function($scope, $http, ourData, $window
           $window.location.href = "#/userprofile";
         }else{
           var errorText = "";
-          for(var i = 0; i < data.errors.length; i++){
-            errorText += data.errors[i] + "\n";
+          for(var i = 0; i < response.errors.length; i++){
+            errorText += response.errors[i] + "\n";
           }
           alert(errorText);
         }
@@ -401,7 +401,7 @@ TheSceneryapp.controller('companyCont', function($scope, $http, ourData, $window
     }
     $scope.company_website_link = company.website_link
     if ($scope.company_website_link != null && $scope.company_website_link != "" && !$scope.company_website_link.match(/\/\//)){
-      $scope.company_website_link = "//" + $scope.company_website_link
+      $scope.company_website_link = "http://" + $scope.company_website_link
     }
     $(".company-description").append(company.description);
 
--- assets/js/perfAVEDcontroller.js
+++ assets/js/perfAVEDcontroller.js
@@ -76,11 +76,11 @@ TheSceneryapp.controller('perfAVEDcont', function($scope, $http, ourData, $windo
       $scope.thisPerformance = data.data.performance;
       $scope.buyTickets = data.data.performance.ticket_link;
       if (!$scope.buyTickets.match(/\/\//) && $scope.buyTickets != null && $scope.buyTickets != ""){
-        $scope.buyTickets = "//" + $scope.buyTickets;
+        $scope.buyTickets = "http://" + $scope.buyTickets;
       }
       $scope.viewTrailer = data.data.performance.trailer_link;
       if (!$scope.viewTrailer.match(/\/\//) && $scope.viewTrailer != null && $scope.viewTrailer != ""){
-        $scope.viewTrailer = "//" + $scope.viewTrailer;
+        $scope.viewTrailer = "http://" + $scope.viewTrailer;
       }
       // this need to happen whenever a user is logged in and not creating a performance
       if($scope.isLogged() === false){
